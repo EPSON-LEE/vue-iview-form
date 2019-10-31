b08b29b4b428d1137917795f6bbfbca19fa67731
leejiahao <491071291@qq.com>
2019-11-01 01:45:59 +0800

    add: version 1

---
+++ src/views/Index.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="wrapper">
+    <Row>
+      <i-col class="border left" span="8">
+        <form-item-engine :config="config" :rules="rules" :model="formValues" />
+      </i-col>
+      <i-col class="border left" span="8"> </i-col>
+      <i-col class="border" span="8"> </i-col>
+    </Row>
+  </div>
+</template>
+<script>
+import FormItemEngine from "./form-engine";
+export default {
+  components: {
+    FormItemEngine
+  },
+  data() {
+    return {
+      formValues: {
+        name: "11",
+        city: 1,
+        fruit: []
+      },
+      rules: {
+        name: [
+          {
+            required: true,
+            message: "The name cannot be empty",
+            trigger: "blur"
+          }
+        ],
+        city: [
+          {
+            required: true,
+            message: "Please select the city",
+            trigger: "change"
+          }
+        ]
+      },
+      config: [
+        {
+          key: "name",
+          label: "姓名",
+          prop: "name",
+          component: {
+            $type: "Input",
+            value: 1,
+            attrs: {},
+            on: {},
+            props: {
+              placeholder: "请输入姓名"
+            }
+          }
+        },
+        {
+          key: "city",
+          label: "城市",
+          prop: "city",
+          component: {
+            $type: "Select",
+            props: {
+              placeholder: "请选择城市",
+              clearable: true,
+              filterable: true
+            },
+            items: [
+              {
+                value: 1,
+                label: "测试1"
+              },
+              {
+                value: 2,
+                label: "测试2"
+              },
+              {
+                value: 3,
+                label: "测试3"
+              },
+              {
+                value: 4,
+                label: "测试4"
+              },
+              {
+                value: 5,
+                label: "测试5"
+              }
+            ]
+          }
+        },
+        {
+          key: "fruit",
+          label: "种类",
+          prop: "fruit",
+          component: {
+            $type: "CheckboxGroup",
+            props: {},
+            items: [
+              {
+                label: "🍎"
+              },
+              {
+                label: "🍌"
+              },
+              {
+                label: "🍑"
+              },
+              {
+                label: "🍒"
+              },
+              {
+                label: "🍎"
+              },
+              {
+                label: "🍉"
+              },
+              {
+                label: "🍊"
+              }
+            ]
+          }
+        }
+      ]
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+.wrapper {
+  margin: 40px;
+  margin-top: 80px;
+}
+.border {
+  border: 1px solid #dcdee2;
+  height: 80vh;
+  padding: 20px;
+}
+.left {
+  border-right: none;
+}
+</style>
---
+++ src/views/form-engine/index.vue
@@ -0,0 +1,233 @@
+<script>
+export default {
+  name: "Internal-Input",
+  props: {
+    // 数据模型
+    model: {
+      type: Object,
+      default() {
+        return {};
+      }
+    },
+    rules: {
+      type: Object,
+      requried: true
+    },
+    config: {
+      type: Array,
+      requried: false
+    }
+  },
+
+  data() {
+    return {
+      updating: false,
+      formValues: this.mergeValues()
+    };
+  },
+
+  watch: {
+    formValues: {
+      handler(formValues) {
+        this.updating = true;
+        this.$emit("input", formValues);
+      },
+      deep: true
+    },
+    model: {
+      handler() {
+        if (!this.updating) {
+          this.formValues = this.mergeValues();
+        } else {
+          this.updating = false;
+        }
+      },
+      deep: true
+    }
+  },
+  render(h) {
+    let vm = this;
+    // 具体渲染逻辑
+    return h(
+      "Form",
+      {
+        props: {
+          model: vm.formValues,
+          rules: vm.rules,
+          labelWidth: 80
+        }
+      },
+      [
+        ...(vm.$slots.prepend || []),
+        ...(vm.renderFormItems(h) || []),
+        ...(vm.$slots.append || [])
+      ]
+    );
+  },
+  created() {
+    this.$emit("input", this.formValues);
+  },
+  methods: {
+    mergeValues() {
+      const vm = this;
+      const { model } = vm;
+      const defaultValues = {};
+      this.config.forEach(({ tag, detail = {} }) => {
+        console.log(tag);
+        let { defaultValue = null, name } = detail;
+        // if (tag === "el-checkbox" || (tag === "el-select" && detail.multiple)) {
+        //   defaultValue = defaultValue != null ? defaultValue : [];
+        // }
+        defaultValues[name] = defaultValue;
+      });
+
+      return {
+        ...defaultValues,
+        ...model
+      };
+    },
+    filterAttrs(detail = {}) {
+      const keys = Object.keys(detail);
+      const attrs = {};
+      keys.forEach(key => {
+        const value = detail[key];
+        if (
+          typeof value === "number" ||
+          typeof value === "string" ||
+          typeof value === "boolean"
+        ) {
+          attrs[key] = value;
+        }
+      });
+      return attrs;
+    },
+    renderFormItem(h, component, label, formItemName) {
+      const vm = this;
+      const tag = component.$type;
+      const { formValues } = vm;
+      const { props } = component;
+      const value = formValues[formItemName] || null;
+      const modelEvents = {
+        input: function(value) {
+          formValues[name] = value;
+        }
+      };
+      let children = [];
+
+      if (tag === "Select") {
+        const select = h(
+          tag,
+          {
+            attrs: {
+              ...vm.filterAttrs(props)
+            },
+            props: {
+              value,
+              ...props
+            },
+            on: {
+              ...modelEvents
+            }
+          },
+          (component.items || []).map(option => {
+            return h("Option", {
+              attrs: {
+                ...vm.filterAttrs(option)
+              },
+              props: {
+                key: option.value,
+                ...option
+              }
+            });
+          })
+        );
+        children = [select];
+      } else if (tag === "CheckboxGroup") {
+        const checkbox = h(
+          "CheckboxGroup",
+          {
+            attrs: {
+              ...vm.filterAttrs(props)
+            },
+            props: {
+              value,
+              ...props
+            },
+            on: {
+              ...modelEvents
+            }
+          },
+          (component.items || []).map(option => {
+            return h("Checkbox", {
+              attrs: {
+                ...vm.filterAttrs(option)
+              },
+              props: {
+                key: option.value,
+                label: option.label
+              }
+            });
+          })
+        );
+        children = [checkbox];
+      } else if (tag === "el-radio") {
+        const radios = (props.items || []).map(option => {
+          option = {
+            name: props.name,
+            ...option
+          };
+          return h(
+            tag,
+            {
+              attrs: {
+                ...vm.filterAttrs(option)
+              },
+              props: {
+                value,
+                ...option
+              },
+              on: {
+                ...modelEvents
+              }
+            },
+            [option.text]
+          );
+        });
+        children = [...radios];
+      } else {
+        const input = h(tag || "Input", {
+          attrs: {
+            ...vm.filterAttrs(props)
+          },
+          props: {
+            value,
+            ...props
+          },
+          on: {
+            ...modelEvents
+          }
+        });
+        children = [input];
+      }
+
+      return h(
+        "FormItem",
+        {
+          props: {
+            prop: name,
+            label: label
+          }
+        },
+        children
+      );
+    },
+
+    renderFormItems(h) {
+      const vm = this;
+      return this.config.map(item => {
+        return vm.renderFormItem(h, item.component, item.label, item.key);
+      });
+    }
+  }
+};
+</script>
